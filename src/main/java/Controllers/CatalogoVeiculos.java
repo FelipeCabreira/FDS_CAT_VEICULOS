5f37341c7f7ca3850c2d3049aeb54c9375376d0c
felipe_cabreira@dell.com <felipe_cabreira@dell.com>
2021-03-21 16:12:56 -0300

    added custom list from external file using scanner

--- src/main/java/Controllers/CatalogoVeiculos.java
+++ src/main/java/Controllers/CatalogoVeiculos.java
@@ -1,14 +1,52 @@
 package Controllers;
 
 
-import Models.Transaction.VeiculoPassageiros;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class CatalogoVeiculos {
+    private int countVeiculos;
+    private ArrayList<String> listVeiculos = new ArrayList<String>();
 
     public CatalogoVeiculos() {
-        VeiculoPassageiros vp;
-        vp = new VeiculoPassageiros();
-        System.out.println("INSTANCIA DE VP" + vp);
+        countVeiculos = 0;
+
+        try {
+            System.out.println(">>>>> Class instancied");
+            System.out.println(">>>>> ######");
+            System.out.println(">>>>> ######");
+            File file = new File("C:\\Users\\felipe_cabreira\\IdeaProjects\\PUCRS\\FDS\\src\\main\\resources\\DB_Veiculos.txt");
+            System.out.println(">>>>> File: ... ---- " + file.getName());
+            System.out.println(">>>>> Reading file from: ... ---- " + file.getPath());
+            ArrayList<String> names = new ArrayList<String>();
+            Scanner in = new Scanner(file);
+            System.out.println(">>>>> Scanner instancied");
+            System.out.println(">>>>> Reading file");
+
+            while (in.hasNextLine()) {
+                countVeiculos++;
+                names.add(in.nextLine());
+            }
+            System.out.println("A total of "+ countVeiculos + " vehicles were added to the list");
+            System.out.println(">>>>> Finished");
+            System.out.println(">>>>> #######");
+            System.out.println(">>>>> #######");
+            System.out.println(">>>>> #######");
+            System.out.println(">>>>> #######");
+            System.out.println(">>>>> Sort file");
+            Collections.sort(names);
+            for (int i = 0; i < names.size(); ++i) {
+                System.out.println(names.get(i));
+            }
+            System.out.println(">>>>> Finished");
+
+
+        } catch (IOException io) {
+            System.out.println("Error: " + io.getMessage());
+        }
     }
 
 
--- src/main/java/FdsCatVeiculos.java
+++ src/main/java/FdsCatVeiculos.java
@@ -3,6 +3,5 @@ import Controllers.CatalogoVeiculos;
 public class FdsCatVeiculos {
     public static void main(String[] args) {
         CatalogoVeiculos cv = new CatalogoVeiculos();
-        System.out.println("Instancia de Catalogo Veiculos" + cv );
     }
 }
