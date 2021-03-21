d68424d3c694fc1920ce5634f484cb177397c85f
jorgegrlucas <jorgegrlucas@gmail.com>
2021-03-21 18:11:05 +0000

    Teste Jorgin Up

--- src/main/java/FdsCatVeiculos.java
+++ src/main/java/FdsCatVeiculos.java
@@ -4,5 +4,6 @@ public class FdsCatVeiculos {
     public static void main(String[] args) {
         CatalogoVeiculos cv = new CatalogoVeiculos();
         System.out.println("Instancia de Catalogo Veiculos" + cv );
+        System.out.println("Teste Jorginho");
     }
 }
